0db10731d2bef83fa85892971f8af2a22cf74e54
gauravkhanna <gauravdkhanna@live.com>
2014-05-01 15:00:31 +0530

    lock/unlock mode

--- myfiles.php
+++ myfiles.php
@@ -19,15 +19,22 @@ $num_rows = mysql_num_rows($result);
 echo("<a href=\"logout.php\">Sign out</a>");
 if($num_rows>0) {
 echo("You can upload one by clicking <a href=\"upload.html\">here</a><br/>");
-echo("<table width=\"1000\"><th>Filename</th><th>Extension</th><th>Upload date</th><th>Last updated</th><th>Protection</th><th>Download</th><th>Check log</th>");
+echo("<table width=\"1000\"><th>Filename</th><th>Extension</th><th>Protection Mode</th><th>Created on</th><th>Last updated</th><th>Download</th><th>Check log</th>");
 while($tuple=mysql_fetch_object($result)) {
 //echo var_dump($tuple);
 echo("<tr>");
 echo("<td>$tuple->filename</td>");
 echo("<td>$tuple->filetype</td>");
+if($tuple->storagetype==0)
+	{
+	echo("<td><img src=\"siteImages/lock.png\"></td>");
+	}
+else
+	{
+	echo("<td><img src=\"siteImages/unlock.png\"></td>");
+	}
 echo("<td>$tuple->createddate</td>");
 echo("<td>$tuple->updatedate</td>");
-echo("<td>$tuple->storagetype</td>");
 echo("<td><form action=\"downloadfile.php\" method=\"POST\"><input type=\"hidden\" name=\"fileid\" value=\"$tuple->fileid\"><input type=\"image\" src=\"siteImages/downloadbutton.jpg\" alt=\"Download\" /></form></td>");
 echo("<td><form action=\"log.php\" method=\"POST\"><input type=\"hidden\" name=\"logid\" value=\"$tuple->logid\"><input type=\"submit\" value=\"See log\"></form></td>");
 echo("</tr>");
